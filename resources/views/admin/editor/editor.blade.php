838a7eb4c43019ef27fc5993e49c13b9e7929b21
toanhditedu <toanhd@tinhvan.com>
2018-08-03 14:06:58 +0700

    update code wyswyg

--- resources/views/admin/editor/editor.blade.php
+++ resources/views/admin/editor/editor.blade.php
@@ -1,19 +1,22 @@
 @extends('admin.home')
 
 @section('javascrip')
-<script src="{{ asset('js/ckeditor/ckeditor.js')}}"></script>
-<script src="{{ asset('js/ckfinder/ckfinder.js')}}"></script>
-<script type="text/javascript">
-    var baseURL = "{!! url('/') !!}";
-</script>
-<script src="{{ asset('js/func_ckfinder.js')}}"></script>
+<script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+<!-- <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=2it6o345n9m69rt8fldr7z4pqq61l14ty56xvd2q076vo1y3"></script> -->
+
 @endsection
 
 @section('content')
 <div>
 	<textarea id="ckeditor-text"></textarea>
 	<script type="text/javascript">
-        ckeditor("ckeditor-text");
+        CKEDITOR.replace('ckeditor-text' );
     </script>
 </div>
+<!-- <div style="margin-top: 20px">
+    <textarea id="ckeditor-text1"></textarea>
+	<script type="text/javascript">
+        tinymce.init({ selector:'#ckeditor-text1' });
+    </script>
+</div> -->
 @endsection
