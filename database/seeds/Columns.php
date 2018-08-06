65e372ef5239bda1f2d8d627949b6041ada2f4ef
TINHVAN\khoidh <chantroibinhyentk5l@gmail.com>
2018-08-06 19:45:10 +0700

    bugfix "add category, edit bcrypt and image "

--- database/seeds/Columns.php
+++ database/seeds/Columns.php
@@ -15,9 +15,29 @@ class Columns extends Seeder
             DB::table('columns')->insert([
                 'id'            => $i,
                 'category_id'   => 1,
-                'title'         => bcrypt('1．日本語教師　2．総務（労務）　 ※オンライン面接可　'),
-                'description'   => bcrypt('高度なIT人材育成のための日本語教師と、当社初の専任総務を募集。'),
-                'image'         => bcrypt('mail_syoku_img_file_1579.jpg'),
+                'title'         => '1．日本語教師　2．総務（労務）　 ※オンライン面接可　',
+                'description'   => '高度なIT人材育成のための日本語教師と、当社初の専任総務を募集。',
+                'image'         => 'column01.jpg',
+                'sort'          => 1,
+            ]);
+        }
+        for($i=21;$i<=40;$i++) {
+            DB::table('columns')->insert([
+                'id'            => $i,
+                'category_id'   => 2,
+                'title'         => 'ジョブシャドウイングを映像化するハタチのトビラとは',
+                'description'   => '弊社では、企業側の「自社の魅力を学生に訴求できていない」といった問題意識や、大学生側の「世の中にどんな仕事があるのか、よくわからない」「説明会や採用ページをみるだけでは、情報として不足している」といった声に応えるために、ジョブシャドウイングの様子を映像化して学生に訴求する「ハタチのトビラ」というサービスを展開しています。',
+                'image'         => 'column02.jpg',
+                'sort'          => 1,
+            ]);
+        }
+        for($i=41;$i<=60;$i++) {
+            DB::table('columns')->insert([
+                'id'            => $i,
+                'category_id'   => 3,
+                'title'         => 'インターンシップの成果指標をどこに置くか？',
+                'description'   => '採用活動の成果指標は一般的に「採用目標数の達成」に重きが置かれ、インターンシップの成果指標は「母集団形成、その先の内定に至った学生の数」に重きが置かれます。',
+                'image'         => 'column01.jpg',
                 'sort'          => 1,
             ]);
         }
--- database/seeds/Videos.php
+++ database/seeds/Videos.php
@@ -16,11 +16,30 @@ class Videos extends Seeder
                 'id'            => $i,
                 'category_id'   => 1,
                 'url'           => 'https://i.ytimg.com/vi_webp/ObwNpMXlmPU/maxresdefault.webp',
-                'description'   => bcrypt('部活の先輩後輩のキス【ファーストキス】。'),
-                'image'         => 'maxresdefault.jpg',
+                'description'   => '部活の先輩後輩のキス【ファーストキス】。',
+                'image'         => 'video01.jpg',
                 'sort'          => 1,
             ]);
         }
-
+        for($i=21;$i<=40;$i++) {
+            DB::table('videos')->insert([
+                'id'            => $i,
+                'category_id'   => 2,
+                'url'           => 'blob:https://www.youtube.com/fffc5984-677e-4cbd-bab3-d205eaedde49',
+                'description'   => 'ハタチのトビラ#3 （株）Huber. 〜インバウンド事業に取り組む社会人の1日にとは〜',
+                'image'         => 'video02.jpg',
+                'sort'          => 1,
+            ]);
+        }
+        for ($i = 41; $i <= 60; $i++) {
+            DB::table('videos')->insert([
+                'id'            => $i,
+                'category_id'   => 3,
+                'url'           => 'blob:https://www.youtube.com/5db0ab72-7d59-42e3-b571-7ceca817b758',
+                'description'   => 'ハタチのトビラ#6 NPO法人グリーンズ〜持続可能な社会をつくるために活動するNPO職員の1日とは〜',
+                'image'         => 'video03.jpg',
+                'sort'          => 1,
+            ]);
+        }
     }
 }
