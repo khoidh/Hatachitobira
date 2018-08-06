31950509503a8fb85ea6aa3ebcbef89dcdf7abb1
Hungtm <manhhungvoi@gmail.com>
2018-08-06 17:34:53 +0900

    filter user video

--- app/Http/Controllers/User/VideoController.php
+++ app/Http/Controllers/User/VideoController.php
@@ -5,35 +5,67 @@ namespace App\Http\Controllers\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 Use App\Video;
+Use App\Category;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 
 class VideoController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        /*
+         *  API Youtube information
+         *
+         */
         $api_key = env('YOUTUBE_API_KEY');
+        $BASE_URL = 'https://www.googleapis.com/youtube/v3/videos?id=';
+        $BASE_PART = '&part=id,contentDetails,snippet,statistics,player&key=';
+        /**/
 
+        $categories = Category::all();
         $videos = Video::all();
+        /*Filter*/
+        if($request->isMethod('post'))
+        {
+            if($request->category_id)
+            {
+                $category_id = $request->category_id;
+                $videos = Video::where('category_id',$category_id)->get();
+            }
+            else if($request->description)
+            {
+                $description = $request->description;
+                $videos = Video::where('description','LIKE',"%$description%")->get();
+            }
+
+        }
+        /*End filter*/
+
         $results = array();
         foreach ($videos as $video)
         {
             $url = $video->url;
             parse_str(parse_url($url, PHP_URL_QUERY), $youtube);
             $id =  $youtube['v'];
-            $api_url = 'https://www.googleapis.com/youtube/v3/videos?id=' . $id . '&part=id,contentDetails,snippet,statistics,player&key=' . $api_key . '';
+            $api_url = $BASE_URL . $id . $BASE_PART . $api_key . '';
             $result = json_decode(file_get_contents($api_url));
             array_push($results,$result);
         }
+
+
         /*Pagination */
+
         $currentPage = LengthAwarePaginator::resolveCurrentPage();
         $itemCollection = collect($results);
+        /*Sau này sẽ sửa perPage = 9*/
         $perPage = 9;
         $currentPageItems = $itemCollection->slice(($currentPage * $perPage) - $perPage, $perPage)->all();
         $paginatedItems= new LengthAwarePaginator($currentPageItems , count($itemCollection), $perPage);
         $paginatedItems->setPath('video');
+        /*End pagination*/
 
-        return view('user.video.index', ['videos' => $videos, 'results' => $paginatedItems]);
+
+        return view('user.video.index', ['videos' => $videos, 'results' => $paginatedItems,'categories'=>$categories]);
 //        return view('user.video.index', ['videos' => $videos, 'results' => $results]);
     }
 
