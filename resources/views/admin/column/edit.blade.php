d89df23974ecb9e1adb285cc9a529d054b554ac7
DAM HUY KHOI <khoidh@tinhvan.com>
2018-08-14 21:03:47 +0700

    [Admin] Add design layout, menu (WIP)

--- resources/views/admin/column/edit.blade.php
+++ resources/views/admin/column/edit.blade.php
@@ -1,7 +1,34 @@
 @extends('admin.home')
 @section('javascrip')
-<script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+    <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
 @endsection
+
+@section('content-header')
+    <div class="content-header-left col-md-6 col-12 mb-2 breadcrumb-new">
+        <h3 class="content-header-title mb-0 d-inline-block" style="font-size: 30px">Columns</h3>
+        <div class="row breadcrumbs-top d-inline-block">
+            <div class="breadcrumb-wrapper col-12">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.html">Home</a>
+                    </li>
+                    <li class="breadcrumb-item active">Drag &amp; Drop
+                    </li>
+                </ol>
+            </div>
+        </div>
+    </div>
+    <div class="content-header-right col-md-6 col-12">
+        <div class="dropdown float-md-right">
+            <button class="btn btn-danger dropdown-toggle round btn-glow px-2" id="dropdownBreadcrumbButton" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Actions</button>
+            <div class="dropdown-menu" aria-labelledby="dropdownBreadcrumbButton"><a class="dropdown-item" href="#"><i class="la la-calendar-check-o"></i> Calender</a>
+                <a class="dropdown-item" href="#"><i class="la la-cart-plus"></i> Cart</a>
+                <a class="dropdown-item" href="#"><i class="la la-life-ring"></i> Support</a>
+                <div class="dropdown-divider"></div><a class="dropdown-item" href="#"><i class="la la-cog"></i> Settings</a>
+            </div>
+        </div>
+    </div>
+@endsection
+
 @section('content')
     <form action="{{route('columns.update',$column->id)}}" method="post">
         {{ csrf_field() }}
