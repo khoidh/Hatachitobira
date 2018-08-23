c11bd5952f1eee76c2a76454bf2a27a27cc0c344
Hungtm <manhhungvoi@gmail.com>
2018-08-23 13:32:05 +0900

    create migration update user

---
+++ database/migrations/2018_08_23_041841_update_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Users', function (Blueprint $table) {
+            $table->string('verifyToken')->after('password');
+            $table->boolean('status')->after('verifyToken');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Users', function (Blueprint $table) {
+            $table->dropColumn('verifyToken');
+            $table->dropColumn('status');
+
+        });
+    }
+}
