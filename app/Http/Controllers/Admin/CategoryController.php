da3331228934ee01f3e06a9a576f508c3e8e98e7
toanhditedu <toanhd@tinhvan.com>
2018-08-27 12:29:09 +0700

    add code category manager

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Category;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::paginate('10');
+        return view('admin.categories.index',compact('categories'));
+    }
+
+    public function create()
+    {
+        return view('admin.categories.addnew');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        Category::create($data);
+        return redirect()->route('categories.index');
+    }
+
+    public function show($id)
+    {
+        $categories = Category::find($id);
+        return view('admin.categories.detail_model',compact('categories'));
+    }
+
+    public function edit($id)
+    {
+       
+    }
+
+    public function update(Request $request, $id)
+    {
+        
+    }
+
+    public function destroy($id)
+    {
+        Category::find($id)->delete();
+        return redirect()->route('categories.index');
+    }
+}
