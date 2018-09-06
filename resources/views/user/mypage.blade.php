acd398ebc40fa963c2a820b38c68a6046df2c3ad
toanhditedu <toanhd@tinhvan.com>
2018-09-06 17:07:11 +0700

    update code fixbug mypage for new wireframe

--- resources/views/user/mypage.blade.php
+++ resources/views/user/mypage.blade.php
@@ -6,80 +6,143 @@
 <div class="row">
     <h3>あなたを形作るマイテーマワードを記入しよう</h3>
     <div class="container mypage">
+        <div class="event-information">
+            <a class="a-user" href="#">このパーツの使い方はこちら</a>
+        </div>
+        <div class="event-information select-month"> 
+            <h4><i class="fa fa-caret-left fa-3x" aria-hidden="true"></i><span class="text-month">2018年9月</span></h4>
+        </div>
+        <div class="event-information">
+            <div class="col-md-2">今月のマイテーマ：</div>
+            <div class="col-md-9">
+                <input type="text" name="" style="width: 100%">
+            </div>
+            <div class="col-md-8">実現するために行う予定の行動目標：</div>
+            <div class="col-md-11">
+                <input type="text" name="" style="width: 100%">
+            </div>
+        </div>
     	<div class="event-information">
+            <div class="event-information-wrapper col-lg-4 col-sm-6 col-xs-12">
+                <div class="event-information-content">
+                    <div class="number">
+                        <span>1</span>
+                    </div>
+                    <div class="mypage-text">
+                        <span>テキストテキストテ
+                                キストテキストテキ
+                                ストテキストテキスト
+                                テキスト</span>
+                        <div class="favorite edit">
+                            <i class="fa fa-pencil" data-toggle="modal" data-target="#show-detail-mypage"> Edit</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="event-information-wrapper col-lg-4 col-sm-6 col-xs-12">
+                <div class="event-information-content">
+                    <div class="number">
+                        <span>2</span>
+                    </div>
+                    <div class="mypage-text">
+                        <span></span>
+                        <div class="favorite edit">
+                            <i class="fa fa-pencil" data-toggle="modal" data-target="#show-detail-mypage"> Edit</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="event-information-wrapper col-lg-4 col-sm-6 col-xs-12">
+                <div class="event-information-content">
+                    <div class="number">
+                        <span>3</span>
+                    </div>
+                    <div class="mypage-text">
+                        <span></span>
+                        <div class="favorite edit">
+                            <i class="fa fa-pencil" data-toggle="modal" data-target="#show-detail-mypage"> Edit</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="event-information-wrapper col-lg-4 col-sm-6 col-xs-12">
+                <div class="event-information-content">
+                    <div class="number">
+                        <span>4</span>
+                    </div>
+                    <div class="mypage-text">
+                        <span></span>
+                        <div class="favorite edit">
+                            <i class="fa fa-pencil" data-toggle="modal" data-target="#show-detail-mypage"> Edit</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
     		<div class="event-information-wrapper col-lg-4 col-sm-6 col-xs-12">
     			<div class="event-information-content">
     				<div class="event-image">
-    					
 	    				<img src="{{ asset('image/column/column02.jpg')}}">
-    				</div>
+                    </div>
     			</div>
     		</div>
-    		<div class="event-information-wrapper col-lg-4 col-sm-6 col-xs-12">
-    			<div class="event-information-content">
-    				<div class="event-image">
-    					
-	    				<img src="{{ asset('image/column/column02.jpg')}}">
-    				</div>
-    			</div>
-    		</div>
-    		<div class="event-information-wrapper col-lg-4 col-sm-6 col-xs-12">
-    			<div class="event-information-content">
-    				<div class="event-image">
-    					
-	    				<img src="{{ asset('image/column/column02.jpg')}}">
-    				</div>
-    			</div>
-    		</div>
-    		<div class="event-information-wrapper col-lg-4 col-sm-6 col-xs-12">
-    			<div class="event-information-content">
-    				<div class="event-image">
-    					
-	    				<img src="{{ asset('image/column/column02.jpg')}}">
-    				</div>
-    			</div>
-    		</div>
-    		<div class="event-information-wrapper col-lg-4 col-sm-6 col-xs-12">
-    			<div class="event-information-content">
-    				<div class="event-image">
-    					
-	    				<img src="{{ asset('image/column/column02.jpg')}}">
-    				</div>
-    			</div>
-    		</div>
-    		<div class="event-information-wrapper col-lg-4 col-sm-6 col-xs-12">
-    			<div class="event-information-content">
-    				<div class="event-image">
-    					
-	    				<img src="{{ asset('image/column/column02.jpg')}}">
-    				</div>
-    			</div>
-    		</div>
-    		<div class="event-information-wrapper col-lg-4 col-sm-6 col-xs-12">
-    			<div class="event-information-content">
-    				<div class="event-image">
-    					
-	    				<img src="{{ asset('image/column/column02.jpg')}}">
-    				</div>
-    			</div>
-    		</div>
-    		<div class="event-information-wrapper col-lg-4 col-sm-6 col-xs-12">
-    			<div class="event-information-content">
-    				<div class="event-image">
-    					
-	    				<img src="{{ asset('image/column/column02.jpg')}}">
-    				</div>
-    			</div>
-    		</div>
-    		<div class="event-information-wrapper col-lg-4 col-sm-6 col-xs-12">
-    			<div class="event-information-content">
-    				<div class="event-image">
-    					
-	    				<img src="{{ asset('image/column/column02.jpg')}}">
-    				</div>
-    			</div>
-    		</div>
-    	</div>
+            <div class="event-information-wrapper col-lg-4 col-sm-6 col-xs-12">
+                <div class="event-information-content">
+                    <div class="number">
+                        <span>5</span>
+                    </div>
+                    <div class="mypage-text">
+                        <span></span>
+                        <div class="favorite edit">
+                            <i class="fa fa-pencil" data-toggle="modal" data-target="#show-detail-mypage"> Edit</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="event-information-wrapper col-lg-4 col-sm-6 col-xs-12">
+                <div class="event-information-content">
+                    <div class="number">
+                        <span>6</span>
+                    </div>
+                    <div class="mypage-text">
+                        <span></span>
+                        <div class="favorite edit">
+                            <i class="fa fa-pencil" data-toggle="modal" data-target="#show-detail-mypage"> Edit</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="event-information-wrapper col-lg-4 col-sm-6 col-xs-12">
+                <div class="event-information-content">
+                    <div class="number">
+                        <span>7</span>
+                    </div>
+                    <div class="mypage-text">
+                        <span></span>
+                        <div class="favorite edit">
+                            <i class="fa fa-pencil" data-toggle="modal" data-target="#show-detail-mypage"> Edit</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="event-information-wrapper col-lg-4 col-sm-6 col-xs-12">
+                <div class="event-information-content">
+                    <div class="number">
+                        <span>8</span>
+                    </div>
+                    <div class="mypage-text">
+                        <span></span>
+                        <div class="favorite edit">
+                            <i class="fa fa-pencil" data-toggle="modal" data-target="#show-detail-mypage"> Edit</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">メモ：</div>
+            <div class="col-md-11">
+                <input type="text" name="" style="width: 100%">
+            </div>
+        </div>
+       
     	<div class="event-information">
     		<div class="select-item col-md-3">
     			<select class="">
@@ -194,5 +257,47 @@
     		</div>
     	</div>
     </div>
+</div>
+<div id="show-detail-mypage" class="modal fade modal_register" role="dialog">
+    <div class="modal-dialog" style="margin-top:150px">
+        <div class="modal-content">
+            <div class="modal-body" style="text-align:center">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <div class="panel-body">
+                    <div class="event-information-wrapper col-md-4 col-md-offset-4 clearfix">
+                        <div class="event-information-content">
+                            <div class="number">
+                                <span class="mypage-number">1</span>
+                            </div>
+                            <div class="mypage-text">
+                                <span>テキストテキストテ
+                                        キストテキストテキ
+                                        ストテキストテキスト
+                                        テキスト</span>
+                                
+                            </div>
+                        </div>
+                        <div class="title-detail">
+                            <span class="mypage-number">1</span> の要素を深掘り
+                        </div>
+                    </div>
+                    <div class="col-md-12 detail-infor">
+                        <div class="event-information-content col-md-5 col-md-offset-1">
+                            テキストテキストテ キストテキスト
+                        </div>
+                         <div class="event-information-content col-md-5 col-md-offset-1">
+                            テキストテキストテ キストテキスト
+                        </div>
+                         <div class="event-information-content col-md-5 col-md-offset-1">
+                            テキストテキストテ キストテキスト
+                        </div>
+                         <div class="event-information-content col-md-5 col-md-offset-1">
+                            テキストテキストテ キストテキスト
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>   
 </div>
 @endsection
