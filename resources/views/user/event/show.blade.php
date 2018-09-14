996220fc256653f385c9d8201f3b09d9a5520b44
DAM HUY KHOI <khoidh@tinhvan.com>
2018-09-14 19:06:02 +0700

    User Edit UI Column/Event detail

--- resources/views/user/event/show.blade.php
+++ resources/views/user/event/show.blade.php
@@ -4,14 +4,51 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/top.css') }}" rel="stylesheet">
     <link href="{{ asset('css/event.css') }}" rel="stylesheet">
-    <link rel='stylesheet' id='style-css' href="{{ asset('css/style.2.2.1.css') }}" type='text/css' media='all'/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel='stylesheet' id='style-css' href="{{ asset('css/style.2.2.1.css') }}" type='text/css' media='all'/>
+    <script src="https://originalpoint.co.jp/wp-content/themes/orion_tcd037/footer-bar/footer-bar.js?ver=2.2.1"></script>
+    <link rel="stylesheet" media="screen and (max-width:770px)" href="https://originalpoint.co.jp/wp-content/themes/orion_tcd037/footer-bar/footer-bar.css?ver=2.2.1">
+    <link rel="stylesheet" media="screen and (max-width:770px)" href="{{asset('css/responsive.css?ver=2.2.1')}}">
+
     <style>
         .fa-heart-o{
             cursor: pointer !important;
         }
         .favorite{
             height: 0 !important;
+        }
+
+        .content-header {
+            position: relative !important;
+
+        }
+
+        .absolute {
+            z-index: 1;
+            position: absolute;
+            background: #5B9BD5;
+            left: 35%;
+            right: 35%;
+            top: -10px;
+            padding: 8px 0 8px 0;
+            border-radius: 4px;
+            text-align: center;
+        }
+
+        .date,
+        .icon{
+            float: right;
+            /*text-align: right;*/
+        }
+        .date{
+            padding: 5px 5px 5px 10px;
+        }
+
+        /*Cho điện laptop, ipad*/
+        @media (min-width: 768px ) {
+            .content-header-right {
+                padding-top: 25px;
+            }
         }
     </style>
 @endsection
@@ -40,79 +77,100 @@
                 <div id="article">
                     <ul id="post_meta_top" class="clearfix">
                     </ul>
-                    <h2 id="post_title" class="rich_font">{{$event->title}}</h2>
+                    {{--<h2 id="post_title" class="rich_font">{{$event->title}}</h2>--}}
 
                     {{--Tieu de tren noi dung--}}
-                    <div class="row" style="line-height: 200%;">
-                        <div class="col-md-6">
-                            {{--<a href="#">--}}
-                            @php $image='image/event/'.$event->image; @endphp
-                            <img width="100%" src="{{file_exists($image)?asset($image): asset('image/event/event_default.jpg')}}">
-                            {{--</a>--}}
+                    <div class="row content-header">
+                        <div class="absolute bg-primary">
+                            <p class="title">イベント</p>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            @php $image='image/event/'.$event->image; @endphp
+                            <img width="100%"
+                                 src="{{file_exists($image)?asset($image): asset('image/event/event_default.jpg')}}">
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-xs-12 content-header-right">
                             <div class="item">
                                 <div class="wrapper">
-                                    <div class="content" >
-                                        {{--<a href="#">--}}
-                                        {{--<div class="status"><a href="#"><h4>インタビュー</h4></a></div>--}}
-                                        {{--<div class="status"><h4>インタビュー</h4></div>--}}
-                                        <div class="title" style="color: #216A94;"><h4 style="line-height: 200%">{{$event->title}}</h4></div>
-                                        {{--                                            <div class="title"><p>{{$event->title}}</p></div>--}}
+                                    <div class="content">
+                                        <div class="title" style="color: #216A94;"><h4
+                                                    style="line-height: 200%">{{$event->title}}</h4></div>
                                         <div class="category"><p>{{$event->category_name}}</p></div>
-                                        <div class="date"><p>{{$event->created_at}}</p></div>
-                                        {{--</a>--}}
-                                    </div>
-                                    <div class="icon">
-
-                                        {{--==================== favorite ====================--}}
-                                        @if(Auth::user())
-                                            {{ csrf_field() }}
-                                            <div type="submit" class="favorite">
-                                                <input type="hidden" class="favorite" value="0">
-                                                <input type="hidden" class="user_id" value="{{Auth::user()->id}}">
-                                                <input type="hidden" class="event_id" value="{{$event->id}}">
-                                                {{--<input type="hidden" class="$favorites_id" value="{{$event->id}}">--}}
-                                                @if(in_array($event->id,$favorites_id))
-                                                    <i class="fa fa-heart-o" style="font-size:24px; color: red;" ></i>
+                                        <div class="row">
+                                            <div class="date" ><p>{{date('Y-m-d', strtotime($event->created_at))}}</p></div>
+                                            <div class="icon" >
+                                                {{--==================== favorite ====================--}}
+                                                @if(Auth::user())
+                                                    {{ csrf_field() }}
+                                                    <div type="submit" class="favorite">
+                                                        <input type="hidden" class="favorite" value="0">
+                                                        <input type="hidden" class="user_id" value="{{Auth::user()->id}}">
+                                                        <input type="hidden" class="event_id" value="{{$event->id}}">
+                                                        @if(in_array($event->id,$favorites_id))
+                                                            <i class="fa fa-heart-o" style="font-size:24px; color: red;"></i>
+                                                        @else
+                                                            <i class="fa fa-heart-o" style="font-size:24px; color: blue;"></i>
+                                                        @endif
+                                                    </div>
                                                 @else
-                                                    <i class="fa fa-heart-o" style="font-size:24px; color: blue;"></i>
+                                                    <div type="submit">
+                                                        <i class="fa fa-heart-o" style="font-size:24px; color: blue;"
+                                                           data-toggle="modal"
+                                                           data-target="#modal_login"> </i>
+                                                    </div>
                                                 @endif
-                                            </div>
-                                        @else
-                                            <div type="submit">
-                                                <i class="fa fa-heart-o" style="font-size:24px; color: blue;" data-toggle="modal"
-                                                   data-target="#modal_login"> </i>
-                                            </div>
-                                        @endif
-                                        {{--==================== /end favorite ====================--}}
+                                                {{--==================== /end favorite ====================--}}
 
+                                            </div>
+
+                                        </div>
                                     </div>
-
                                 </div>
                             </div>
                         </div>
                     </div>
 
-
-                    <div class="post_content clearfix">
-                        {!! $event->content !!}
+                    {{--noi dung--}}
+                    <div class="row content-body" style="margin-top: 10px; margin-bottom: 10px">
+                        <div class="post_content clearfix">
+                            {{--{!! $event->content !!}--}}
+                            <img width="100%" src="{{asset('image/event_detail.jpg')}}" alt="event_detail">
+                        </div>
                     </div>
 
-                    <div id="previous_next_post" class="clearfix">
+                    <p>
+                        @php
+                            $time_now = Carbon\Carbon::now();
+                            $time_from = Carbon\Carbon::parse($event->time_from);
+                            $time_to = Carbon\Carbon::parse($event->time_to);
+                            $check=$time_now->between($time_from,$time_to);
+                            if($check)
+                                $event_state="申し込む";
+                            else
+                                $event_state="受付終了";
+                        @endphp
+                        <button type="button" class="btn btn-primary center-block pl-2 pl-2">{{$event_state}}</button>
+                    </p>
+                    <p style="padding-bottom:30px "></p>
+
+                    <hr />
+
+                    <div id="previous_next_post" class="clearfix" style="padding-top: 10px; margin-bottom: 10px">
                         <div class="prev_post"><p class="label">PREV</p><a href="#"
                                                                            title="{{$previous->title}}"><img
                                         width="200" height="200" src="#"
                                         class="attachment-size1 size-size1 wp-post-image" alt=""
                                         srcset="https://originalpoint.co.jp/wp-content/uploads/2017/12/c478b1a6dedb96603a486610161ab963-200x200.jpg 200w, https://originalpoint.co.jp/wp-content/uploads/2017/12/c478b1a6dedb96603a486610161ab963-150x150.jpg 150w, https://originalpoint.co.jp/wp-content/uploads/2017/12/c478b1a6dedb96603a486610161ab963-120x120.jpg 120w"
-                                        sizes="(max-width: 200px) 100vw, 200px"><span class="title">{{$previous->title}}</span></a>
+                                        sizes="(max-width: 200px) 100vw, 200px"><span
+                                        class="title">{{$previous->title}}</span></a>
                         </div>
                         <div class="next_post"><p class="label">NEXT</p><a href="{{route('event.show', $next->id)}}"
                                                                            title="{{$next->title}}"><img
                                         width="200" height="200" src="#"
                                         class="attachment-size1 size-size1 wp-post-image" alt=""
                                         srcset="https://originalpoint.co.jp/wp-content/uploads/2018/02/IMG_0873-200x200.jpg 200w, https://originalpoint.co.jp/wp-content/uploads/2018/02/IMG_0873-150x150.jpg 150w, https://originalpoint.co.jp/wp-content/uploads/2018/02/IMG_0873-120x120.jpg 120w"
-                                        sizes="(max-width: 200px) 100vw, 200px"><span class="title">{{$next->title}}</span></a>
+                                        sizes="(max-width: 200px) 100vw, 200px"><span
+                                        class="title">{{$next->title}}</span></a>
                         </div>
                     </div>
 
@@ -132,7 +190,8 @@
                                                                                                   https://originalpoint.co.jp/wp-content/uploads/2017/10/a6354a2f677436097ccd391f43cb9a76-300x207.jpg 300w"
                                                                                                    sizes="(max-width: 500px) 100vw, 500px"></a>
                                 <div class="desc">
-                                    <h4 class="title"><a href="{{route('event.show', $event->id)}}" name="">{{str_limit($event->title,$limit=50,$end='...')}}</a>
+                                    <h4 class="title"><a href="{{route('event.show', $event->id)}}"
+                                                         name="">{{str_limit($event->title,$limit=50,$end='...')}}</a>
                                     </h4>
                                 </div>
 
@@ -140,12 +199,8 @@
                         @endforeach
                     </ol>
                 </div>
-
-
                 <!-- banner2 -->
-
             </div><!-- END #left_col -->
-
 
             <div id="side_col">
                 <div class="side_widget clearfix widget_search" id="search-2">
@@ -200,14 +255,13 @@
                                         allowfullscreen="true" scrolling="no" allow="encrypted-media"
                                         id="fb_xdm_frame_https" aria-hidden="true"
                                         title="Facebook Cross Domain Communication Frame" tabindex="-1"
-                                        src="https://staticxx.facebook.com/connect/xd_arbiter/r/QX17B8fU-Vm.js?version=42#channel=f321bcfe3351984&amp;origin=https%3A%2F%2Foriginalpoint.co.jp"
+                                        {{--src="https://staticxx.facebook.com/connect/xd_arbiter/r/QX17B8fU-Vm.js?version=42#channel=f321bcfe3351984&amp;origin=https%3A%2F%2Foriginalpoint.co.jp"--}}
                                         style="border: none;" __idm_frm__="1410"></iframe>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div><!-- END #main_col -->
     </div>
 
