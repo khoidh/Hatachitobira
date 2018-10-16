bbcb93202a9aa63989f96aa70fef683188aacd77
toanhditedu <toanhd@tinhvan.com>
2018-10-16 08:29:32 +0700

    add code table categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $table = 'categories';
-    protected $fillable = ['name','description'];
+    protected $fillable = ['name','description','slug','icon','sort','display'];
 
     public function columns()
     {
