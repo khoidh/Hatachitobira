16e67fe04265840bb058bd3f4db349baebf22da0
Tu Manh Hung <manhhungvoi@gmail.com>
2018-10-26 18:23:23 +0900

    fix migration remove after table

--- database/migrations/2018_10_25_032907_update_mytheme_table.php
+++ database/migrations/2018_10_25_032907_update_mytheme_table.php
@@ -14,21 +14,21 @@ class UpdateMythemeTable extends Migration
     public function up()
     {
         Schema::create('my_themes', function (Blueprint $table) {
-             $table->increments('id');
+            $table->increments('id');
             $table->integer('user_id')->unsigned();
-            $table->integer("category_id")->after('id')->comment("カテゴリ");
+            $table->integer("category_id")->comment('カテゴリ');
             $table->string('memo',256)->nullable();
             $table->string('last_log',256)->nullable();
             $table->text('keywords',256)->nullable();
             $table->string('this_mytheme',256)->nullable();
             $table->string('this_action',256)->nullable();
-            $table->string("content_lable",255)->nullable()->after('this_action');
-            $table->string("content_1",255)->nullable()->after('content_lable');
-            $table->string("content_2",255)->nullable()->after('content_1');
-            $table->string("content_3",255)->nullable()->after('content_2');
-            $table->string("content_4",255)->nullable()->after('content_3');
-            $table->integer("month")->after('content_4');
-            $table->integer("year")->after('month');
+            $table->string("content_lable",255)->nullable();
+            $table->string("content_1",255)->nullable();
+            $table->string("content_2",255)->nullable();
+            $table->string("content_3",255)->nullable();
+            $table->string("content_4",255)->nullable();
+            $table->integer("month");
+            $table->integer("year");
             $table->timestamps();
         });
     }
