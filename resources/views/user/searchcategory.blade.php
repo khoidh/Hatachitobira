6360f40df0a9935d44f6947e51eab7594dc784d4
toanhditedu <toanhd@tinhvan.com>
2018-10-30 15:08:25 +0700

    update code fix bug

--- resources/views/user/searchcategory.blade.php
+++ resources/views/user/searchcategory.blade.php
@@ -312,37 +312,37 @@
         });
 
         $(document).on('click','.video .video-list .browse-details', function(e){
-                e.preventDefault();
-                var idvideo = $(this).data('id');
-                var src = $(this).data('src');
-                var url = $(this).data('url');
-                $('#modal_video .twitter').attr('href','https://twitter.com/intent/tweet?url='+url);
-                $('#modal_video .facebook').attr('href','https://www.facebook.com/sharer/sharer.php?u='+url);
-                $('#modal_video .panel-body').html(src);
-                $('#modal_video').modal('show');
-            });
+            e.preventDefault();
+            var idvideo = $(this).data('id');
+            var src = $(this).data('src');
+            var url = $(this).data('url');
+            $('#modal_video .twitter').attr('href','https://twitter.com/intent/tweet?url='+url);
+            $('#modal_video .facebook').attr('href','https://www.facebook.com/sharer/sharer.php?u='+url);
+            $('#modal_video .panel-body').html(src);
+            $('#modal_video').modal('show');
+        });
 
-            var popupMeta = {
-                width: 400,
-                height: 400
+        var popupMeta = {
+            width: 400,
+            height: 400
+        }
+        $(document).on('click', '.social-share', function(event){
+            event.preventDefault();
+
+            var vPosition = Math.floor(($(window).width() - popupMeta.width) / 2),
+                hPosition = Math.floor(($(window).height() - popupMeta.height) / 2);
+
+            var url = $(this).attr('href');
+            var popup = window.open(url, 'Social Share',
+                'width='+popupMeta.width+',height='+popupMeta.height+
+                ',left='+vPosition+',top='+hPosition+
+                ',location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1');
+
+            if (popup) {
+                popup.focus();
+                return false;
             }
-            $(document).on('click', '.social-share', function(event){
-                event.preventDefault();
-
-                var vPosition = Math.floor(($(window).width() - popupMeta.width) / 2),
-                    hPosition = Math.floor(($(window).height() - popupMeta.height) / 2);
-
-                var url = $(this).attr('href');
-                var popup = window.open(url, 'Social Share',
-                    'width='+popupMeta.width+',height='+popupMeta.height+
-                    ',left='+vPosition+',top='+hPosition+
-                    ',location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1');
-
-                if (popup) {
-                    popup.focus();
-                    return false;
-                }
-            });
+        });
 
         $(document).on('change','.select-box.search',function(e){
             console.log('aaaaa');
