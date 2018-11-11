24ed015bb27a392cc67619ad5c8dc6e3ae6e0cf9
Trung H. Tran <tranhoangtrung.nob@gmail.com>
2018-11-12 01:13:26 +0900

    Add file storage for column, ediotr, event

--- resources/views/admin/column/create.blade.php
+++ resources/views/admin/column/create.blade.php
@@ -5,6 +5,14 @@
 @endsection
 @section('javascrip')
     <script src= "{{asset("vendor/unisharp/laravel-ckeditor/ckeditor.js")}}"></script>
+    <script>
+      var options = {
+        filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
+        filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
+        filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
+        filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
+      };
+    </script>
 @endsection
 
 @section('content-header')
@@ -70,7 +78,7 @@
             <textarea class="form-control" name="content" placeholder="Content" id="ckeditor-text" >{{old('content')}}</textarea>
         </div>
         <script type="text/javascript">
-            CKEDITOR.replace('ckeditor-text' );
+            CKEDITOR.replace('ckeditor-text', options);
         </script>
     </div>
 
--- resources/views/admin/editor/editor.blade.php
+++ resources/views/admin/editor/editor.blade.php
@@ -3,6 +3,14 @@
 @section('javascrip')
 <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
 <!-- <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=2it6o345n9m69rt8fldr7z4pqq61l14ty56xvd2q076vo1y3"></script> -->
+<script>
+  var options = {
+    filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
+    filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
+    filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
+    filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
+  };
+</script>
 
 @endsection
 
@@ -10,7 +18,7 @@
 <div>
 	<textarea id="ckeditor-text"></textarea>
 	<script type="text/javascript">
-        CKEDITOR.replace('ckeditor-text' );
+        CKEDITOR.replace('ckeditor-text', options);
     </script>
 </div>
 <!-- <div style="margin-top: 20px">
