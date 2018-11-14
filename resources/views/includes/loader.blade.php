02e0797aca27eba75c7eff1ef0cb810680a30bbb
Yusuke Mizukoshi <u2k.hommerice@gmail.com>
2018-11-15 02:52:56 +0900

    fix loader

--- resources/views/includes/loader.blade.php
+++ resources/views/includes/loader.blade.php
@@ -1,4 +1,4 @@
-<div id="loading col-md-12 col-12" style="display: none;">
+<div id="loading" class="col-md-12 col-12">
     {{-- <div class="loading-overlay"></div> --}}
     <div class="loader">
         <div class="loader-inner ball-spin-fade-loader">
