f64f899d6860163a648585b591a98e7ba41ce0d5
Trung H. Tran <tranhoangtrung.nob@gmail.com>
2018-11-14 15:57:05 +0900

    Add loader to layout html

---
+++ resources/views/includes/loader.blade.php
@@ -0,0 +1,15 @@
+<div id="loading">
+    <div class="loading-overlay"></div>
+    <div class="loader">
+        <div class="loader-inner ball-spin-fade-loader">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+</div>
