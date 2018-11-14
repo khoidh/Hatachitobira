7a9b41e0620175511ac84bd8aee354cab20c7b6e
Trung H. Tran <tranhoangtrung.nob@gmail.com>
2018-11-14 23:01:45 +0900

    Remove overlay and change color of loader

--- resources/views/includes/loader.blade.php
+++ resources/views/includes/loader.blade.php
@@ -1,5 +1,5 @@
 <div id="loading">
-    <div class="loading-overlay"></div>
+    {{-- <div class="loading-overlay"></div> --}}
     <div class="loader">
         <div class="loader-inner ball-spin-fade-loader">
             <div></div>
