d9cecc5a4c80f55a9bf456afa9560610839ce312
Trung H. Tran <tranhoangtrung.nob@gmail.com>
2018-11-20 22:36:05 +0900

    Fix bug after merge

--- resources/views/user/mypage-text.blade.php
+++ resources/views/user/mypage-text.blade.php
@@ -4,7 +4,7 @@
     </div>
 </div>
 <div class="event-information-wrapper col-md-12 clearfix">
-    <textarea style="width: 100%;border: none;" type="text" rows="3" id="action-of-month" class="input-action" data-month="{{isset($mytheme_first->month) ? $mytheme_first->month : $data['month']}}" data-value="{{$data['field']}}"
+    <textarea style="width: 100%;border: none;" type="text" rows="3" id="model-textarea-edit" class="input-action" data-month="{{isset($mytheme_first->month) ? $mytheme_first->month : $data['month']}}" data-value="{{$data['field']}}"
                         data-year="{{isset($mytheme_first->year) ? $mytheme_first->year : $data['year']}}" placeholder="{{$data['placehoder']}}"
                         data-type = "{{$data['typies']}}">{{$data['field']}}</textarea>
 
