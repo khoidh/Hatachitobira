ca2d84d5272bec137c40133c5456cdf73b0676d2
Trung H. Tran <tranhoangtrung.nob@gmail.com>
2018-11-20 17:38:58 +0900

    Fix video type not display concept_movie

--- app/Http/Controllers/User/VideoController.php
+++ app/Http/Controllers/User/VideoController.php
@@ -16,7 +16,7 @@ class VideoController extends Controller
 
     public function index(Request $request)
     {
-        $video_types = VideoType::where('id','!=',3)->get();
+        $video_types = VideoType::where('video_types.id', '!=', Video::CONCEPT_MOVIE_TYPE)->get();
         $videos = Video::select()
             ->select('videos.*','video_types.name as category_name')
             ->join('video_types','video_types.id','=','videos.type')
@@ -64,7 +64,7 @@ class VideoController extends Controller
     public function videoSearchCategory(Request $request) {
         $data = $request->all();
         
-        $categories = VideoType::all();
+        $video_types = VideoType::where('video_types.id', '!=', Video::CONCEPT_MOVIE_TYPE)->get();
         $videos = Video::select()
             ->select('videos.*','video_types.name as category_name')
             ->join('video_types','video_types.id','=','videos.type')
